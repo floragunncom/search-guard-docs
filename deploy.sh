d894a5256b2796aca40166b6b8581ff1fa71b5ba
Lyudmila Huzenko <lhuzenko@search-guard.com>
2020-06-18 15:45:12 +0300

    [DEVOPS-64] Deploying files to new webserver

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,15 @@ rm -rf ./_site
 bundle install
 bundle exec jekyll build --config _config.yml,_versions.yml
 
-ncftpput -R -v -u $ftp_username -p $ftp_password 35.214.156.137  /latest ./_site/*
+echo "Checking access to webserver"
+ssh -T cirunner@185.233.188.50
+
+echo "Copying files to webserver"
+rsync -r ./_site "cirunner@185.233.188.50:/usr/share/nginx/html/_site/docs/$VERSION"
+
+echo "Relinking the current latest version as the latest link"
+ssh cirunner@185.233.188.50 'cd /usr/share/nginx/html/_site/docs/; latest_version=${ls -la ./|grep -v latest|sort|uniq|tail -n 1}; ln -s $latest_version latest'
+
 export GIT_COMMIT_DESC=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 echo "Last commit message: $GIT_COMMIT_DESC"
 if [[ $GIT_COMMIT_DESC == *"noindex"* ]]; then
