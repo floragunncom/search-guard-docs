64dcf66f8992866141fc6e3042cbd7721b6d41db
Jochen Kressin <jkressin@floragunn.com>
2020-07-07 14:55:59 +0200

    downgrade from /latest

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ rm -rf ./_site
 bundle install
 bundle exec jekyll build --config _config.yml,_versions.yml
 
-ncftpput -R -v -u $ftp_username -p $ftp_password 35.214.156.137  /latest ./_site/*
+ncftpput -R -v -u $ftp_username -p $ftp_password 35.214.156.137  /7.x-42 ./_site/*
 export GIT_COMMIT_DESC=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 echo "Last commit message: $GIT_COMMIT_DESC"
 if [[ $GIT_COMMIT_DESC == *"noindex"* ]]; then
