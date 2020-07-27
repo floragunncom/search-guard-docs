5257aefb1c86a9cf31023836c119ee6ac6686e7b
Lyudmila Huzenko <lhuzenko@search-guard.com>
2020-07-27 19:55:29 +0300

    Adding linking to latest version correct folder

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ rsync -r ./_site/* "cirunner@185.233.188.50:/data/www/docs/$VERSION" || exit 1
 #rsync -r ./_site/* "cirunner@185.233.188.50:/data/www/docs/" || exit 1
 echo "Relinking the current version as the latest in docs"
 
-ssh cirunner@185.233.188.50 'cd /usr/share/nginx/html/_site/docs/; latest_version=`ls -l ./|grep -v latest|grep -v total|cut -f9 -d" "|sort|uniq|tail -n 1`; ln -s $latest_version latest' || exit 1
+ssh cirunner@185.233.188.50 'cd /data/www/docs/; rm -r latest || true ; latest_version=`ls -l ./|grep -v total|cut -f9 -d" "|sort|uniq|tail -n 1`; ln -s $latest_version latest' || exit 1
 
 export GIT_COMMIT_DESC=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 echo "Last commit message: $GIT_COMMIT_DESC"
